b434a89411f11e915edadb3cdc6d5cf3f5e55962
Sula <holmes1067@gmail.com>
2020-12-06 03:02:55 +0600

    Separate scraping of product's characteristics

--- Program.cs
+++ Program.cs
@@ -46,20 +46,79 @@ public class Program
         {
             var htmlDocLinks = htmlWeb.Load(itemLink);
 
-            var nodeLinks = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li");
 
-            var priceLinks = htmlDocLinks.DocumentNode.SelectNodes("//div[@title='Цена в интернет-магазине']");
+            //Model names of GPU
+            var gpuModelName = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Модель')]");
 
-
-
-
-            foreach (var linksGPU in nodeLinks)
+            foreach (var linksGPU in gpuModelName)
             {
-                Console.WriteLine(linksGPU.InnerText.Trim());
+                Console.WriteLine(linksGPU.InnerText);
             }
-            foreach (var priceGPU in priceLinks)
+
+           
+
+            //Frequency of GPUs
+            var gpuVideoFrequency = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Частота видеопроцессора')]");
+
+            foreach (var linksGPU in gpuVideoFrequency)
             {
-                Console.WriteLine(priceGPU.InnerText + "\n");
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Memory Frequency of GPUs
+            var gpuMemoryFrequency = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Частота видеопамяти')]");
+
+            foreach (var linksGPU in gpuMemoryFrequency)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Video Memory type
+            var gpuMemoryType = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Тип видеопамяти')]");
+
+            foreach (var linksGPU in gpuMemoryType)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Video Memory size
+            var gpuMemorySize = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Объем видеопамяти')]");
+
+            foreach (var linksGPU in gpuMemorySize)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Video Memory type
+            var gpuMemoryWidth = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Разрядность шины видеопамяти')]");
+
+            foreach (var linksGPU in gpuMemoryWidth)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Universal processor count
+            var gpuProcessorCount = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Количество универсальных')]");
+
+            foreach (var linksGPU in gpuProcessorCount)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Connection type
+            var gpuConnectionType = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li[contains(text(), 'Разъемы')]");
+
+            foreach (var linksGPU in gpuConnectionType)
+            {
+                Console.WriteLine(linksGPU.InnerText);
+            }
+
+            //Prices
+            var productPrices = htmlDocLinks.DocumentNode.SelectNodes("//div[@title='Цена в интернет-магазине']");
+
+            foreach (var linksGPU in productPrices)
+            {
+                Console.WriteLine(linksGPU.InnerText + "\n");
             }
 
         }
@@ -71,48 +130,48 @@ public class Program
 
 
 
-        while (Loop == true)
-        {
+        //while (Loop == true)
+        //{
 
-            var NextPageCheck = htmlDoc.DocumentNode.SelectNodes("//li[@class='bx-pag-next']/a").ToList();
+        //    var NextPageCheck = htmlDoc.DocumentNode.SelectNodes("//li[@class='bx-pag-next']/a").ToList();
 
-            if (NextPageCheck.Count != 0)
-            {
-                string link = url + NextPageCheck[0].Attributes["href"].Value;
+        //    if (NextPageCheck.Count != 0)
+        //    {
+        //        string link = url + NextPageCheck[0].Attributes["href"].Value;
 
-                var urlDecode = HttpUtility.HtmlDecode(link);
-                htmlDoc = htmlWeb.Load(urlDecode);
-
-
-                htmlDoc = htmlWeb.Load(link);
-
-                foreach (var itemLink in result1)
-                {
-                    var htmlDocLinks = htmlWeb.Load(itemLink);
-
-                    var nodeLinks = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li");
-
-                    var priceLinks = htmlDocLinks.DocumentNode.SelectNodes("//div[@title='Цена в интернет-магазине']");
+        //        var urlDecode = HttpUtility.HtmlDecode(link);
+        //        htmlDoc = htmlWeb.Load(urlDecode);
 
 
+        //        htmlDoc = htmlWeb.Load(link);
+
+        //        foreach (var itemLink in result1)
+        //        {
+        //            var htmlDocLinks = htmlWeb.Load(itemLink);
+
+        //            var nodeLinks = htmlDocLinks.DocumentNode.SelectNodes("//ul[@class='bx-short-desc']/li");
+
+        //            var priceLinks = htmlDocLinks.DocumentNode.SelectNodes("//div[@title='Цена в интернет-магазине']");
 
 
-                    foreach (var linksGPU in nodeLinks)
-                    {
-                        Console.WriteLine(linksGPU.InnerText.Trim());
-                    }
-                    foreach (var priceGPU in priceLinks)
-                    {
-                        Console.WriteLine(priceGPU.InnerText + "\n");
-                    }
 
-                }
-            }
-            else
-            {
-                Loop = false;
-            }
-            }
+
+        //            foreach (var linksGPU in nodeLinks)
+        //            {
+        //                Console.WriteLine(linksGPU.InnerText.Trim());
+        //            }
+        //            foreach (var priceGPU in priceLinks)
+        //            {
+        //                Console.WriteLine(priceGPU.InnerText + "\n");
+        //            }
+
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Loop = false;
+        //    }
+        //    }
 
 
 
